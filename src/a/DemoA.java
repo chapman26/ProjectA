f35f5363e581e330803c7203133b398c6d2582f3
samchapman <samchapman@192.168.1.245>
2024-01-07 23:54:13 -0500

    abc123

---
+++ src/a/DemoA.java
@@ -0,0 +1,13 @@
+package a;
+
+public class DemoA {
+	public static void main(String[] args) {
+
+		System.out.println("Hello again A");
+		
+		System.out.println("Testing it out");
+		
+		System.out.println("Testing out again");
+	}
+
+}
